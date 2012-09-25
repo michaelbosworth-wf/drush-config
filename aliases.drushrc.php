e6795ce8eec3a4c215027dd842b6d826e0a42801
Michael Bosworth <michael.bosworth@webfilings.com>
2012-09-25 10:52:17 -0500

    initial commit

---
+++ aliases.drushrc.php
@@ -0,0 +1,33 @@
+<?php
+
+$aliases['www'] = array(
+  'uri' => 'local.webfilings.com',
+  'root' => '/mnt/Sites/webfilings.com',
+  'remote-host' => 'webdev10.04',
+  'remote-user' => 'boz',
+);
+
+$aliases['mobile'] = array(
+  'uri' => 'local-mobile.webfilings.com',
+  'root' => '/mnt/Sites/mobile.webfilings.com',
+  'remote-host' => 'webdev',
+  'remote-user' => 'boz',
+);
+
+$aliases['connect'] = array(
+  'uri' => 'local-connect.webfilings.com',
+  'root' => '/mnt/Sites/connect.webfilings.com',
+  'remote-host' => 'webdev',
+  'remote-user' => 'boz',
+);
+
+$aliases['help'] = array(
+  'uri' => 'local-help.webfilings.com',
+  'root' => '/mnt/Sites/help.webfilings.com',
+  'remote-host' => 'webdev',
+  'remote-user' => 'boz',
+);
+
+
+
+?>
